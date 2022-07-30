81a0214cef1bae0670af0cb6bbf1fc09d3a278b6
tech5hu <shuweynaahmed@gmail.com>
2022-07-30 23:16:38 +0100

    date

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-let btnShow = document.querySelector('button');
-let output = document.querySelector('h1');
-btnShow.addEventListener('click', () => 
-{let today = new Date();
-    let month = today.getMonth() + 1;
-    let year = today.getFullYear();
-    let date = today.getDate();
-    let current_date = `${month}/${date}/${year}`;
-    output.innerText = current_date;});
-
+const d = new Date();
