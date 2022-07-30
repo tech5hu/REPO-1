fe0541db468fa23b2fff8605f54421c8f2b29aaf
tech5hu <shuweynaahmed@gmail.com>
2022-07-30 23:01:02 +0100

    date

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
+let btnShow = document.querySelector('button');
+let output = document.querySelector('h1');
+btnShow.addEventListener('click', () => {
+    let today = new Date();
+    
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let date = today.getDate();
+    let current_date = `${month}/${date}/${year}`;
+    // output.innerText = current_date;
+    let hours = addZero(today.getHours());
+    let minutes = addZero(today.getMinutes());
+    let seconds = addZero(today.getSeconds());
+    let current_time = `${hours}:${minutes}:${seconds}`;
+    output.innerText = current_time;
+});
+function addZero(num){
+    return num < 10 ? `0${num}`:num;
+}
