35fcfa864879a7b762621d310b576eabe06c69ed
tech5hu <shuweynaahmed@gmail.com>
2022-07-30 23:05:33 +0100

    date

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ btnShow.addEventListener('click', () => {
     let year = today.getFullYear();
     let date = today.getDate();
     let current_date = `${month}/${date}/${year}`;
-    // output.innerText = current_date;
-    let hours = addZero(today.getHours());
-    let minutes = addZero(today.getMinutes());
-    let seconds = addZero(today.getSeconds());
-    let current_time = `${hours}:${minutes}:${seconds}`;
-    output.innerText = current_time;
-});
+    output.innerText = current_date;});
 function addZero(num){
     return num < 10 ? `0${num}`:num;
 }
